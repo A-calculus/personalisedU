690f3b5b210f5f0d33c077d940c699e3f7ccc2ba
Rex <Rex@Damilolas-MacBook-Pro.local>
2025-04-04 04:48:00 -0500

    favicon added

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>PersonalizedU</title>
+        <link rel="icon" href="/logo.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp 
